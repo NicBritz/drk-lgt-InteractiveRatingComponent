533d31e6c746d07f1da7e5a82b52dd64fcfe7d32
nico britz <nico.britz@outlook.com>
2022-08-31 21:59:23 +0100

    :bug: fix ty component import

--- .vercel/output/functions/render.func/.svelte-kit/output/server/nodes/2.js
+++
@@ -1,7 +0,0 @@
-import * as module from '../entries/pages/index.svelte.js';
-
-export { module };
-export const index = 2;
-export const file = '_app/immutable/pages/index.svelte-425ff186.js';
-export const imports = ["_app/immutable/pages/index.svelte-425ff186.js","_app/immutable/chunks/index-367179ca.js","_app/immutable/chunks/index-d400fbb5.js"];
-export const stylesheets = ["_app/immutable/assets/index-1dd29502.css"];
